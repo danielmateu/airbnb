d751b5cb77993f91e3451dc5d7362d8c426e1d97
Daniel Mateu <danielmateu86@gmail.com>
2023-09-05 11:06:18 +0200

    let's try

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
     images: {
         unoptimized: true,
         domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'res.cloudinary.com'],
     },
+    // basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig
